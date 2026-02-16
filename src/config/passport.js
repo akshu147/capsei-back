4ee64108c9e1c093451349dc22d5916c0b6ee468
akshu <145148486+akshu147@users.noreply.github.com>
2026-02-17 01:14:59 +0530

    testing

--- src/config/passport.js
+++ src/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/api/auth/google/callback"
+      callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`
     },
     (accessToken, refreshToken, profile, done) => {
       const user = {
